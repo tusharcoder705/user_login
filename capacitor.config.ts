09b51084635be8eedd3aff37b7e2db66254593b4
Nishant Paliwal <paliwalnishant0@gmail.com>
2026-04-13 14:55:39 +0530

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'ionic-auth-app',
+  webDir: 'dist'
+};
+
+export default config;
